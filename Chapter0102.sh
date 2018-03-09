be4fbbb9db9eb30e3ba9f52f74248bfd67620cf5
dmngit <dmninbox@gmail.com>
2018-03-09 08:34:53 +0100

    printf command

--- Chapter0102.sh
+++ Chapter0102.sh
@@ -1,5 +1,13 @@
+# like printf finction in c
+# %s, %c, %d and %f are format substitution
+# characters
+# - represents the left alignment, 
+# in case it's not specifie content will be
+# aligned to right
+
 
 printf "%-5s %-10s %-4s\n" No Name Mark
 printf "%-5s %-10s %-4.2f\n" 1 Adam 80.3456
 printf "%-5s %-10s %-4.2f\n" 2 Betty 90.9989
 printf "%-5s %-10s %-4.2f\n" 3 Carl 77.564
+
